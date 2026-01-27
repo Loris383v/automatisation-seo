1b99058e180899c357b2fa96f2ffa653bd1bb951
Loris383v <194787933+Loris383v@users.noreply.github.com>
2026-01-27 10:30:38 +0100

    version & changelog

--- auto-seo.php
+++ auto-seo.php
@@ -3,7 +3,7 @@
  * Plugin Name: SEO Automatique
  * Plugin URI: https://github.com/loris383v/automatisation-seo
  * Description: Automatisation de la génération des méta-descriptions et mots-clés pour Yoast, avec IA gratuite.
- * Version: 2.0
+ * Version: 2.0.0
  * Author: Loris Lacote
  * Author URI: https://github.com/loris383v
  * Requires Plugins: wordpress-seo
